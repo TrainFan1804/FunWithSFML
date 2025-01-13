df517abbc312a59c365a7e1da481b83e228c696e
o.le <oleschulz123@gmail.com>
2025-01-13 16:05:31 +0100

    Increase readabilty

--- src/lib/pong/resmg/FontManager.cpp
+++ src/lib/pong/resmg/FontManager.cpp
@@ -11,18 +11,18 @@ bool FontManager::loadResource(int id, const std::string &resource_path)
         throw std::runtime_error("Path is empty");
     }
 
-    auto res = _resource_map.find(id);
-    if (res == _resource_map.end())
+    if(_resource_map.find(id) != _resource_map.end())
     {
-        auto new_resource = std::make_unique<sf::Font>();
-        if (!new_resource->loadFromFile(resource_path))
-        {
-            throw std::runtime_error("Failed to load resource: " + resource_path);
-        }
-        _resource_map.emplace(id, std::move(new_resource));
-        return true;
+        return false;
     }
-    return false;
+
+    auto new_resource = std::make_unique<sf::Font>();
+    if (!new_resource->loadFromFile(resource_path))
+    {
+        throw std::runtime_error("Failed to load resource: " + resource_path);
+    }
+    _resource_map.emplace(id, std::move(new_resource));
+    return true;
 }
 
 sf::Font &FontManager::getResource(int id)
